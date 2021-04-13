60382906a54ee97179b3ca2c977a27cc1700aabb
kens-aais <kens@aaisonline.com>
2021-04-13 11:17:18 -0600

    [1] Updating the ability to load secrets from AWS

--- openidl-k8s/load-secrets.js
+++ openidl-k8s/load-secrets.js
@@ -3,13 +3,17 @@
  * call via node load-secrets.js <cloud> <environment> <node>
  * The results will be placed into the config directory.
  */
+const { exit } = require('node:process')
 const secretsManager = require('../openidl-secrets-manager/secrets-manager')
 
 var args = process.argv.slice(2)
-if (args.length !== 3) {
-    console.log('Error.  Must provide 3 parguments: cloud, environment, node.')
-    console.log("Usage: node index.js <cloud> <environment> <node>")
+
+if (args.length < 3) {
+    console.log('Error.  Must provide at least 3 parguments: cloud, environment, node.')
+    console.log("Usage: node load-secrets.js <cloud> <environment> <node>")
     exit(1)
 } else {
-    secretsManager.loadSecretsFromCloud('./charts/openidl-secrets/config', args[0], args[1], args[2])
+    var destination = (args.length > 3 ? args[3] : './charts/openidl-secrets/config')
+    secretsManager.loadSecretsFromCloud(destination, args[0], args[1], args[2])
+    exit(0)
 }
