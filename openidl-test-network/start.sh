be53d8b182f7ad3c985310852e962b424e2cfbe0
leo <leo.wu@itpeoplecorp.com>
2021-08-02 14:24:23 -0400

    add fabric local network

---
+++ openidl-test-network/start.sh
@@ -0,0 +1,23 @@
+# Pull the images
+./bootstrap.sh 2.2.3 1.5.0
+
+# bring down the current network
+./network.sh down
+
+# bring up the network
+./network.sh up -ca -s couchdb
+
+# create the defaultchannel
+./network.sh createChannel -c defaultchannel -p DefaultChannel
+
+# create the analytics-aais
+./network.sh createChannel -c analytics-aais -p AnalyticsAaisChannel
+
+# create the analytics-carrier
+./network.sh createChannel -c analytics-carrier -p AnalyticsCarrierChannel
+
+# deploy the chaincode to all the peers
+./network.sh deployCC -c defaultchannel -ccn openidl-cc-default -ccp ./openidl/ -ccl go
+
+# Pre-register user openidl-aais-data-call-processor-ibp-2.0
+./pre-registerUser.sh
