5ccea596c8acf8d3539d1d48e34ad286cad23617
Sandeep Pulluru <sandeep.pulluru@ibm.com>
2021-08-02 21:58:35 -0400

    updated startup script to include local network setup

--- systemup.sh
+++ systemup.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 echo "Here we setup the system from start to finish."
+
+echo "Setting up hyperledger fabric local test network"
+cd openidl-test-network
+./start.sh
+cd ..
+
+echo "Setting up minikube"
 make delete_minikube
 make start_minikube
+
+echo "Build local images for minikube"
+eval $(minikube -p minikube docker-env)
+./build-local-images.sh
+
+echo "Enable ingress and update hosts"
 make enable_ingress
 ./updatehosts.sh
-eval $(minikube -p minikube docker-env)
+
+echo "Check local images for minikube"
 docker images | grep openidl
+
+echo "Copy hyperledger fabric local test network connection profile"
+cp -R ./openidl-test-network/organizations/peerOrganizations/aais.example.com/connection-aais.json ./openidl-k8s/charts/openidl-secrets/config/connection-profile.json 
+
+echo "Install application helm charts"
 # make load_images
 make install_in_k8s
