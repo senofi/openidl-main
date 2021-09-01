aa1d9c0d5ecd8a1c99157daefa3f0da248f8af4f
Sandeep Pulluru <sandeep.pulluru@chainyard.com>
2021-09-01 15:12:33 -0400

    updated register users to include carrier-insurance-data-manager

--- openidl-test-network/pre-register-users.sh
+++ openidl-test-network/pre-register-users.sh
@@ -182,3 +182,21 @@ data="${initCommand/signcerts/$signingIdentity}"
 data="${data/privatekey/$signingPrivKey}"
 signcertsJSON="{\"data\":$data}"
 curl -X PUT http://admin:adminpw@localhost:9984/wallet/"openidl-carrier-data-call-processor-ibp-2.0" -d "$signcertsJSON"
+
+# register user - openidl-carrier-insurance-data-manager-ibp-2.0
+fabric-ca-client register --caname ca-carrier --id.name openidl-carrier-insurance-data-manager-ibp-2.0 --id.secret password --id.type client --id.attrs 'orgType=advisory:ecert' --tls.certfiles ${PWD}/organizations/fabric-ca/carrier/tls-cert.pem
+
+# enroll user
+fabric-ca-client enroll -u https://openidl-carrier-insurance-data-manager-ibp-2.0:password@localhost:7054 --caname ca-carrier -M ${PWD}/organizations/peerOrganizations/carrier.example.com/users/openidl-carrier-insurance-data-manager-ibp-2.0@carrier.example.com/msp --tls.certfiles ${PWD}/organizations/fabric-ca/carrier/tls-cert.pem
+cp ${PWD}/organizations/peerOrganizations/carrier.example.com/msp/config.yaml ${PWD}/organizations/peerOrganizations/carrier.example.com/users/openidl-carrier-insurance-data-manager-ibp-2.0@carrier.example.com/msp/config.yaml
+
+# export signcerts to couchdb
+signcerts=$(cat ${PWD}/organizations/peerOrganizations/carrier.example.com/users/openidl-carrier-insurance-data-manager-ibp-2.0@carrier.example.com/msp/signcerts/cert.pem)
+signingIdentity="${signcerts//$'\n'/\\\\n}"
+privKey=$(cat ${PWD}/organizations/peerOrganizations/carrier.example.com/users/openidl-carrier-insurance-data-manager-ibp-2.0@carrier.example.com/msp/keystore/*)
+signingPrivKey="${privKey//$'\n'/\\\\r\\\\n}"
+initCommand='"{\"credentials\":{\"certificate\":\"signcerts\\n\",\"privateKey\":\"privatekey\\r\\n\"},\"mspId\":\"carriermsp\",\"type\":\"X.509\",\"version\":1}"'
+data="${initCommand/signcerts/$signingIdentity}"
+data="${data/privatekey/$signingPrivKey}"
+signcertsJSON="{\"data\":$data}"
+curl -X PUT http://admin:adminpw@localhost:9984/wallet/"openidl-carrier-insurance-data-manager-ibp-2.0" -d "$signcertsJSON"
