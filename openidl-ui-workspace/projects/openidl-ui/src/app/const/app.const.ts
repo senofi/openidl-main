5c4ead34918be1bb4ff0a8b9f01f4802a7e674ab
kens-aais <kens@aaisonline.com>
2021-09-28 16:36:00 -0600

    Testing the image publishing

--- openidl-ui-workspace/projects/openidl-ui/src/app/const/app.const.ts
+++ openidl-ui-workspace/projects/openidl-ui/src/app/const/app.const.ts
@@ -1,18 +1,20 @@
 export const appConst = {
-  regulator: {
-    org: {
-      'ohio doi': '../../assets/images/reg-ohio-doi.png',
-      'colorado doi': '../../assets/images/reg-colorado-doi.png',
-      'virginia doi': '../../assets/images/reg-virginia-doi-new.png',
-      'california doi': '../../assets/images/reg-california-doi.png',
-      'mississippi doi': '../../assets/images/reg-mississippi-doi-new.jpeg',
-      'connecticut doi': '../../assets/images/reg-connecticut-doi.png',
-      'north dakota doi': '../../assets/images/reg-north-dakota-doi-new.jpeg',
-      'alabama doi': '../../assets/images/reg-alabama-doi.jpg',
-      'maryland doi': '../../assets/images/reg-maryland-doi.jpeg',
-      'new jersey doi': '../../assets/images/reg-newjersey-doi.jpg',
-      'texas doi': '../../assets/images/reg-texas-doi.png',
-    },
+	regulator: {
+		org: {
+			'ohio doi': '../../assets/images/reg-ohio-doi.png',
+			'colorado doi': '../../assets/images/reg-colorado-doi.png',
+			'virginia doi': '../../assets/images/reg-virginia-doi-new.png',
+			'california doi': '../../assets/images/reg-california-doi.png',
+			'mississippi doi':
+				'../../assets/images/reg-mississippi-doi-new.jpeg',
+			'connecticut doi': '../../assets/images/reg-connecticut-doi.png',
+			'north dakota doi':
+				'../../assets/images/reg-north-dakota-doi-new.jpeg',
+			'alabama doi': '../../assets/images/reg-alabama-doi.jpg',
+			'maryland doi': '../../assets/images/reg-maryland-doi.jpeg',
+			'new jersey doi': '../../assets/images/reg-newjersey-doi.jpg',
+			'texas doi': '../../assets/images/reg-texas-doi.png'
+		},
 
 		shouldCreateDatacall: true
 	},
@@ -31,6 +33,7 @@ export const appConst = {
 		shouldCreateDatacall: false
 	},
 	roles: {
+		// added for ui role decoration
 		carrier: '../../assets/images/carrier.png',
 		regulator: '../../assets/images/regulator.png',
 		statagent: '../../assets/images/stat-agent.png'
