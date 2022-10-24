74a6511703f2379f5bf5f6693e2bb69783c7ead8
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-10-24 11:40:24 -0400

    update for new data format

--- openidl-extraction-pattern-developer/test/extractionPatterns/nd-extractionPattern.js
+++ openidl-extraction-pattern-developer/test/extractionPatterns/nd-extractionPattern.js
@@ -2,20 +2,21 @@
  * Sample file with map and reduce functions used to create an extraction pattern.
  */
 
- let vins = require("../data/vins.json")
+
 
  function map() {
     let queryMonth = "2022-06"
+    transactionMonth = `${transactionDate.split('-')[0]}-${transactionDate.split('-')[1]}`
     if (transactionMonth == queryMonth){
         emit(
-            this.vinHash, this.transactionMonth
+            this.vinHash, this.transactionDate
         )
     }
 }
 function reduce(key, value) {
     //return {"key": key, "value": value};
 
-    return {"vinHash": key, "expiration": value[0]}
+    return {"vinHash": key, "date": value[0]}
 }
 
 exports.map = map
