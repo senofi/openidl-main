65433508fd2333c2a2360761687e75c263064ed1
Lyubomir Bandrov <lyubomirbandrov@yahoo.com>
2023-02-16 10:09:26 +0200

    IDL-52 Fix event function test

--- openidl-transactional-data-event-listener/server/service/channelTransactionService.js
+++ openidl-transactional-data-event-listener/server/service/channelTransactionService.js
@@ -1,16 +1,24 @@
-const {Transaction} = require('@senofi/openidl-common-lib');
-const networkConfig =require('../config/connection-profile.json');
+const { Transaction } = require('@senofi/openidl-common-lib');
+const networkConfig = require('../config/connection-profile.json');
 
 const targetChannelConfig = require('../config/target-channel-config');
 function createTargetChannelTransactions() {
-    const { org, user, mspId } = targetChannelConfig.users[0];
-    return new Map(
-        targetChannelConfig.targetChannels.map(({channelName, chaincodeName}) => {
-            const targetChannelTransaction = new Transaction(org, user, channelName, chaincodeName, mspId);
-            targetChannelTransaction.init(networkConfig);
-            return [channelName, targetChannelTransaction];
-        })
-    );
+	const { org, user, mspId } = targetChannelConfig.users[0];
+	return new Map(
+		targetChannelConfig.targetChannels.map(
+			({ channelName, chaincodeName }) => {
+				const targetChannelTransaction = new Transaction(
+					org,
+					user,
+					channelName,
+					chaincodeName,
+					mspId
+				);
+				targetChannelTransaction.init(networkConfig);
+				return [channelName, targetChannelTransaction];
+			}
+		)
+	);
 }
 
 module.exports = createTargetChannelTransactions;
