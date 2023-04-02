65fc3cef1a70eb5d08d3d7f5dec89a2552247992
TsvetanG <tsvetan.georgiev@gmail.com>
2023-04-01 21:24:22 -0400

    Update ibp-messages-config.js
    Add retry error message request timeout
    
    Signed-off-by: TsvetanG <tsvetan.georgiev@gmail.com>
    Signed-off-by: Lyubomir Bandrov <lyubomir.bandrov@senofi.ca>

--- openidl-common-lib/helper/config/ibp-messages-config.js
+++ openidl-common-lib/helper/config/ibp-messages-config.js
@@ -1,5 +1,5 @@
 const MESSAGES_CONFIG = {
-    errorMessages: ["Failed to connect before the deadline", 'No peers available to query', 'Unable to connect to the fabric peer service', 'The channel has been closed', 'Endorsement has failed'],
+    errorMessages: ["Failed to connect before the deadline", 'No peers available to query', 'Unable to connect to the fabric peer service', 'The channel has been closed', 'Endorsement has failed', 'REQUEST TIMEOUT'],
     retryCount: 3
 }
 
