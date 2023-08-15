ebb22f6116accaeabe71c0a80a55b2b671367279
Lyubomir Bandrov <lyubomir.bandrov@senofi.ca>
2023-08-15 20:37:05 +0300

    IDL-88 add aws and azure tests
    Signed-off-by: Lyubomir Bandrov <lyubomir.bandrov@senofi.ca>

---
+++ openidl-cloud-services/test/secret/aws-secrets-manager-client.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const AWSSecretsManagerClient = require('../../secret/aws-secrets-manager-client');
+
+describe('AWSSecretsManagerClient', () => {
+    it('should fetch and parse secret value', async () => {
+        const secretsManagerClient = new AWSSecretsManagerClient();
+        const expectedSecretValue = { secretValue: 'secretValue' };
+
+        const mockClient = sinon.stub(secretsManagerClient, '_getClient').returns({
+            getSecretValue: () => {
+                return {
+                    promise: () => {
+                        return Promise.resolve({ SecretString: JSON.stringify(expectedSecretValue) });
+                    }
+                };
+            }
+        });
+
+        const mySecretId = 'my-secret-id';
+        const secret = await secretsManagerClient.getSecret(mySecretId);
+
+        expect(secret).to.deep.equal(expectedSecretValue);
+        expect(secretsManagerClient._getClient.called).to.be.true;
+    });
+});
---
+++ openidl-cloud-services/test/secret/azure-key-vault-secrets-client.test.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { SecretClient } = require('@azure/keyvault-secrets');
+const config = require('config');
+const AzureKeyVaultSecretsClient = require('../../secret/azure-key-vault-secrets-client');
+
+describe('AzureKeyVaultSecretsClient', () => {
+    const configStub = sinon.stub(config, 'get');
+    const secretClientStub = sinon.stub(SecretClient.prototype, 'getSecret').resolves({
+        value: JSON.stringify({ key: 'value' })
+     });
+    beforeEach(() => {
+        configStub.withArgs('azureTenantId').returns('mockedTenantId');
+        configStub.withArgs('azureClientId').returns('mockedClientId');
+        configStub.withArgs('azureClientSecret').returns('mockedClientSecret');
+        configStub.withArgs('azureVaultUrl').returns('mockedVaultUrl');
+    })
+    afterEach(() => {
+        configStub.restore();
+        secretClientStub.restore();
+    })
+    it('should fetch and parse secret value', async () => {
+        const azureKeyVaultSecretsClient = new AzureKeyVaultSecretsClient();
+        const secret = await azureKeyVaultSecretsClient.getSecret('my-secret');
+
+        expect(secret).to.deep.equal({ key: 'value' });
+        expect(SecretClient.prototype.getSecret.called).to.be.true;
+    });
+});
