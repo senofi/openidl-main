51bc5e4b5cc6bb29ddbd7f4421a01c377f5eba66
Lyubomir Bandrov <lyubomirbandrov@yahoo.com>
2023-08-19 13:37:54 +0300

    IDL-88 Fix export of InsuranceDataStoreClientFactory
    Signed-off-by: Lyubomir Bandrov <lyubomir.bandrov@senofi.ca>

--- openidl-cloud-services/index.js
+++ openidl-cloud-services/index.js
@@ -2,11 +2,11 @@ const FileStorageFactory = require(
     './file-storage/file-storage-client-factory');
 const WalletStorageClientFactory = require(
     './wallet-storage/wallet-storage-client-factory');
-const TransactionalDataStorageClientFactory = require(
+const InsuranceDataStoreClientFactory = require(
     './insurance-data-store/insurance-data-store-client-factory');
 
 module.exports = {
   FileStorageFactory,
   WalletStorageClientFactory,
-  TransactionalDataStorageClientFactory
+  InsuranceDataStoreClientFactory
 }
