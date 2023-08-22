04420bdc849bf978e17c5121f8b4f4ee8a6ea0a8
Lyubomir Bandrov <lyubomirbandrov@yahoo.com>
2023-08-22 20:51:05 +0300

    IDL-88 Fix wrong import in tests of cloud service
    Signed-off-by: Lyubomir Bandrov <lyubomir.bandrov@senofi.ca>

--- openidl-transactional-data-event-listener/test/event-function-test.js
+++ openidl-transactional-data-event-listener/test/event-function-test.js
@@ -6,32 +6,35 @@ const expect = chai.expect;
 const config = require('config');
 const openidlCommonLib = require('@senofi/openidl-common-lib');
 const Transaction = openidlCommonLib.Transaction;
-const EventListener = openidlCommonLib.EventListener
+const EventListener = openidlCommonLib.EventListener;
 const eventFunction = require('../server/event/eventHandler').eventFunction;
 const eventPayload = require('./test-data/event-payload.json');
 const getInsuranceDataResponse = require(
-    './test-data/insurance-data-response.json');
+  './test-data/insurance-data-response.json');
 const payload = JSON.stringify(eventPayload);
-const sinon = require("sinon");
-const {TransactionalDataStorageClientFactory} = require(
-    'openidl-common-lib/cloud-services');
+const sinon = require('sinon');
+const { InsuranceDataStoreClientFactory } = openidlCommonLib.CloudServices;
 let targetObject;
 
 describe('TransactionalDataAvailable Event Test', () => {
   before(() => {
     const targetObject = {
-      getTransactionalData: () => Promise.reject("error"),
+      getTransactionalData: () => Promise.reject('error'),
       saveTransactionalData: () => Promise.resolve(
-          "Records Inserted Successfully")
+        'Records Inserted Successfully')
     };
-    sinon.stub(TransactionalDataStorageClientFactory, 'getInstance').resolves(
-        targetObject)
+    sinon.stub(InsuranceDataStoreClientFactory, 'getInstance')
+    .resolves(
+      targetObject);
 
-    sinon.stub(Transaction.prototype, 'executeTransaction').returns(
-        JSON.stringify(getInsuranceDataResponse));
-    sinon.stub(Transaction, 'initWallet').returns(true);
-    sinon.stub(EventListener, 'getCarriersInstance').returns(
-        new Transaction);
+    sinon.stub(Transaction.prototype, 'executeTransaction')
+    .returns(
+      JSON.stringify(getInsuranceDataResponse));
+    sinon.stub(Transaction, 'initWallet')
+    .returns(true);
+    sinon.stub(EventListener, 'getCarriersInstance')
+    .returns(
+      new Transaction);
     console.log('this is target object', targetObject);
   });
 
@@ -40,11 +43,14 @@ describe('TransactionalDataAvailable Event Test', () => {
   });
 
   it('should process TransactionalDataAvailable event ', () => {
-    eventFunction.TransactionalDataAvailable(payload, "5000").then(
-        function (result) {
-          expect(result).to.equal(true);
+    eventFunction.TransactionalDataAvailable(payload, '5000')
+    .then(
+      function (result) {
+        expect(result)
+        .to
+        .equal(true);
 
-        });
+      });
   });
 
 });
