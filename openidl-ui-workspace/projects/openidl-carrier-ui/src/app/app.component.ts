9f8b58ea361048162385f52f1d9b297d667b57e0
Lyubomir Bandrov <lyubomirbandrov@yahoo.com>
2023-09-13 19:48:38 +0300

    IDL-89 Fix not calling configuration of auth service
    Signed-off-by: Lyubomir Bandrov <lyubomir.bandrov@senofi.ca>

--- openidl-ui-workspace/projects/openidl-carrier-ui/src/app/app.component.ts
+++ openidl-ui-workspace/projects/openidl-carrier-ui/src/app/app.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from '../environments/environment';
+import {Component, OnInit} from '@angular/core';
+import {environment} from '../environments/environment';
+import {JwksValidationHandler, OAuthService} from "angular-oauth2-oidc";
+import {AuthConfigService} from "../../../openidl-common-ui/src/lib/services/auth.config.service";
+
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-	title = 'Openidl app';
-	API_ENDPOINT = environment.DATA_CALL_CARRIER_APP_URL;
+  title = 'Openidl app';
+  API_ENDPOINT = environment.DATA_CALL_CARRIER_APP_URL;
 
-	constructor() {
-		localStorage.setItem('API_ENDPOINT', JSON.stringify(this.API_ENDPOINT));
-	}
-	ngOnInit() {
-		sessionStorage.removeItem('isModalOpen');
-	}
+  constructor(private oauthService: OAuthService, private authConfigService: AuthConfigService) {
+    localStorage.setItem('API_ENDPOINT', JSON.stringify(this.API_ENDPOINT));
+  }
+
+  ngOnInit() {
+    sessionStorage.removeItem('isModalOpen');
+
+    this.authConfigService.loadConfig().then(result => {
+      const config = this.authConfigService.getAuthConfig();
+      this.oauthService.configure({
+        ...config,
+        responseType: 'code',
+        tokenEndpoint: config.token_endpoint,
+        userinfoEndpoint: config.userinfo_endpoint,
+        loginUrl: config.authorization_endpoint,
+      });
+      this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+      this.oauthService.tryLogin().then(e => {
+        this.oauthService.setupAutomaticSilentRefresh();
+      })
+    })
+  }
 }
